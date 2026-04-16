6c1226dff29829d3a756e2211ebc9ac9dda0aed0
Douglas <64422034+douglas2990@users.noreply.github.com>
2026-04-15 21:47:59 -0300

    Update WhatsApp support number to a placeholder

--- core/src/main/java/com/example/core/util/ConstantesNumeroCelular.kt
+++ core/src/main/java/com/example/core/util/ConstantesNumeroCelular.kt
@@ -2,5 +2,5 @@ package com.example.core.util
 
 object  ConstantesNumeroCelular {
     // No seu arquivo de constantes
-    const val WHATSAPP_SUPORTE = "5511993825047" // Com o 55 do Brasil e sem espaços/traços
+    const val WHATSAPP_SUPORTE = "5511xxxxxx" // Com o 55 do Brasil e sem espaços/traços
 }
