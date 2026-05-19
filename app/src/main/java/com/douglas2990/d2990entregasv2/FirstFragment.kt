bde4968271d5e0def3acd743fdd6dad6d7dbbd81
Douglas <64422034+douglas2990@users.noreply.github.com>
2026-05-19 12:48:25 -0300

    feature-desenvolvendo app-administrador , melhorando os layouts de app-administrador.

--- app/src/main/java/com/douglas2990/d2990entregasv2/FirstFragment.kt
+++ app/src/main/java/com/douglas2990/d2990entregasv2/FirstFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.douglas2990.d2990entregasv2.databinding.FragmentFirstBinding
+import com.douglas2990.d2990entregasv2.databinding.FragmentFirstMenuBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -16,7 +17,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentFirstMenuBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,7 +28,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentFirstMenuBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -35,9 +36,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+
         binding.buttonCadastrarEmpresaFragment.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_cadastrarEmpresaFragment)
         }
@@ -48,9 +47,7 @@ class FirstFragment : Fragment() {
         binding.buttonMostrarMotoristaCadastradoFragment.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_motoristasCadastradosFragment)
         }
-        binding.buttonCadastrarLoginMotorista.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_cadastroAcessoFragment)
-        }
+
         binding.buttonCadastrarRotaMotorista.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_criarRotaProMotoristaFragment)
         }
